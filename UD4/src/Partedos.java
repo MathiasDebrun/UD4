d578b9b52285dcfa2155f083502e06a2f4736fd4
MathiasDebrun <mathiasdebrun@hotmail.com>
2021-02-02 20:34:55 +0100

    Primer cambio

---
+++ UD4/src/Partedos.java
@@ -0,0 +1,18 @@
+
+public class Partedos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Parte dos
+		
+				int N=5;
+				double A=4.56;
+				char C='a';
+				
+				
+				System.out.println("Variable N= " +N);
+				System.out.println("Variable A= " +A);
+
+	}
+
+}
